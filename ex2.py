835beb7ee9d9187ea49be10203e6ee6ebc987def
Vaschenko Bogdan <bogdan.vaschenko@gmail.com>
2021-03-22 19:53:47 +0300

    All done

--- ex2.py
+++ ex2.py
@@ -7,21 +7,16 @@ class YaUploader:
 
   def upload(self, file_path: str):
     """Метод загруджает файл file_path на яндекс диск"""
-    #ya_path = 'path=netology%5C'
     over_write = '&overwrite=true'
-    stat = requests.get(f'https://cloud-api.yandex.net/v1/disk/resources/upload?path={file_path+over_write}', headers={'Accept': 'application/json', 'Authorization': self.token})
-    print(stat)
+    file_name = file_path.split('\\')[-1]
+    stat = requests.get(f'https://cloud-api.yandex.net/v1/disk/resources/upload?path={file_name+over_write}', headers={'Accept': 'application/json', 'Authorization': self.token})
     send_url = stat.json()['href']
-    file_to_send = {file_path : open(file_path, 'rb')}
-    print(file_to_send)
-    # putting = requests.put(send_url, file_to_send, headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': self.token})
-    puttting = requests.put(send_url, data=file_to_send, headers={'Accept': 'application/json'})
-    print(puttting)
-    # print(puttting.json())
-    return 'Вернуть ответ об успешной загрузке'
+    with open(file_path, 'rb') as file:
+      requests.put(send_url, data=file, headers={'Accept': 'application/json'})
+    return print(f"Файл {file_path} отправлен на Яндекс диск")
 
 
 if __name__ == '__main__':
-  
+
   uploader = YaUploader(t)
-  result = uploader.upload('1.txt')
+  result = uploader.upload("test\\2.txt")
